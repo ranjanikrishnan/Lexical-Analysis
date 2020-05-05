280cc36321d103c0deed5310a3bfbc7d8b01ac95
Ranjani Krishnan <ranjanikrishnanr@gmail.com>
2020-05-05 20:22:43 +0200

    updated core logic for lexical density calculation

---
+++ src/lexical/lexical_density.py
@@ -0,0 +1,19 @@
+from .non_lexicals import non_lexical_words
+
+
+def get_words(input_text):
+    words = input_text.lower().split(' ')
+    return words
+
+
+def get_lexical_words(words):
+    lexical_words = list(filter(lambda x: x not in non_lexical_words, words))
+    return lexical_words
+
+
+def get_lexical_density(input_text):
+    words = get_words(input_text)
+    lexical_words = get_lexical_words(words)
+    lexical_density = len(lexical_words) / len(words)
+    return round(lexical_density, 2)
+
