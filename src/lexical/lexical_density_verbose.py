2aa811ec5e98bd50690a0aa6e0f523d13dd4c0a3
Ranjani Krishnan <ranjanikrishnanr@gmail.com>
2020-05-05 20:23:11 +0200

    Verbose lexical density calculation

---
+++ src/lexical/lexical_density_verbose.py
@@ -0,0 +1,16 @@
+import re
+from .lexical_density import get_lexical_density
+
+
+def get_sentences(input_text):
+    sentences = re.split(r'(?<=[^A-Z].[.?]) +(?=[A-Z])', input_text)
+    return sentences
+
+
+def get_lexical_density_verbose(input_text):
+    sentences = get_sentences(input_text)
+    lexical_density_verbose = list(map(
+        lambda x: get_lexical_density(x), sentences))
+    return lexical_density_verbose
+
+
