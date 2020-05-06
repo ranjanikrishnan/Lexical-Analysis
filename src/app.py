1da380bd318af00e88d0e0960d4efb3a52f107c6
Ranjani Krishnan <ranjanikrishnanr@gmail.com>
2020-05-06 22:21:55 +0200

    modified app.py and redirected routes

--- src/app.py
+++ src/app.py
@@ -1,39 +1,23 @@
-from flask import Flask, request
+from flask import Flask
 from flask_swagger_ui import get_swaggerui_blueprint
-from lexical.compute_lexical_density import compute_ld
+from handlers.routes import configure_routes
 
 app = Flask(__name__)
 
-SWAGGER_URL = '/api/docs'  
-API_URL = '/static/swagger.json'  
+SWAGGER_URL = '/api/docs'
+API_URL = '/static/swagger.json'
 
 swaggerui_blueprint = get_swaggerui_blueprint(
-    SWAGGER_URL,  
+    SWAGGER_URL,
     API_URL,
-    config={  
+    config={
         'app_name': "Lexical Analysis"
     },
 )
 
 app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
-
-@app.route('/')
-def hello():
-    return 'Testing if this shows up! And it does. Yaaaay!'
-
-
-@app.route('/complexity', methods=['POST'])
-def lexical_analysis():
-    try:
-        request_data = request.get_json()
-        input_text = request_data['input_text']
-        mode = request.args.get('mode')
-        computed_ld = compute_ld(mode, input_text)
-        return computed_ld
-    except Exception as e:
-        print(e)
-
+configure_routes(app)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ src/handlers/routes.py
@@ -0,0 +1,19 @@
+from flask import request
+from lexical.compute_lexical_density import compute_ld
+
+
+def configure_routes(app):
+    @app.route('/')
+    def hello():
+        return 'Testing if this shows up! And it does. Yaaaay!'
+
+    @app.route('/complexity', methods=['POST'])
+    def lexical_analysis():
+        try:
+            request_data = request.get_json()
+            input_text = request_data['input_text']
+            mode = request.args.get('mode')
+            computed_ld = compute_ld(mode, input_text)
+            return computed_ld
+        except Exception as e:
+            print(e)
