51e9fae969c8f5ef6f290ff3c2b2ac9d50ebddc4
Ranjani Krishnan <ranjanikrishnanr@gmail.com>
2020-05-09 12:53:52 +0200

    minor modification in seed_db

--- mongo/seed_db.py
+++ mongo/seed_db.py
@@ -1,14 +1,14 @@
+import os
 import pymongo
 import urllib
-from lexical.non_lexicals import non_lexical_words
+from mongo.non_lex import non_lexical_words
 
 
 non_lexical_words = {'words': non_lexical_words}
 
-mongo_uri = "mongodb+srv://ranjani:" + urllib.parse.quote("fenderchamp@26") + "@cluster0-lsocm.mongodb.net/test?retryWrites=true&w=majority"
+mongo_uri = "mongodb+srv://" + os.environ.get('MONGO_USERNAME') + ":" + urllib.parse.quote(os.environ.get('MONGO_PASSWORD')) + "@cluster0-lsocm.mongodb.net/test?retryWrites=true&w=majority"
 client = pymongo.MongoClient(mongo_uri)
 
 lexical_db = client['non_lexicals']
 lexical_collection = lexical_db['non_lexical_words']
-x = lexical_collection.insert_one(non_lexical_words)
-print(x)
+non_lex = lexical_collection.insert_one(non_lexical_words)
